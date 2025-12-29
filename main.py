c616c358fd93a30378a80d14069b2e2fb933378a
hisel-sabermetrics <hisel-sabermetrics.casket645@simplelogin.fr>
2025-12-30 06:14:21 +0800

    perf: rewrite main login

--- main.py
+++ main.py
@@ -5,18 +5,35 @@ Date: 19 Jun 2025
 from argparse import ArgumentParser, FileType
 from atexit import register
 from collections.abc import Iterable, Iterator
-from concurrent.futures import ProcessPoolExecutor
+from concurrent.futures import Future, ProcessPoolExecutor, as_completed, wait
 from difflib import get_close_matches
-from itertools import count, repeat
+from itertools import chain, count, repeat
 from math import inf
 from os import path, system
 from os.path import exists, expanduser
 from re import escape, findall, search, split, sub
 from subprocess import DEVNULL, PIPE, run
-from typing import BinaryIO, Tuple
+from typing import Any, BinaryIO, Optional, Tuple
 
 from ffmpeg import input
-from numpy import append, array, frombuffer, object_, uint8, where
+from numpy import (
+    append,
+    array,
+    bool_,
+    concatenate,
+    diff,
+    flatnonzero,
+    float32,
+    frombuffer,
+    int8,
+    intp,
+    isin,
+    object_,
+    r_,
+    uint8,
+    where,
+)
+from numpy._typing import _32Bit
 from numpy.typing import NDArray
 from ocrmac import ocrmac
 from PIL import Image
@@ -59,14 +76,17 @@ def video_to_frames(
         stderr=DEVNULL,
         shell=True,
     ).stdout
+
     cast_to_ndarray: NDArray = frombuffer(buffer, uint8)
+    del buffer
+
     frame_matrix: NDArray = cast_to_ndarray.reshape(
         (num_frame, height, width, 3)
-    )[frame_index]
+    )
 
     return [
         fromarray(frame_matrix[i, start_y:end_y, start_x:end_x, :])
-        for i in range(num_frame)
+        for i in frame_index
     ]
 
 
@@ -160,6 +180,134 @@ def seg_from_key(video_path_escaped: str, crop) -> str:
         text=True,
         shell=True,
     ).stderr
+
+
+def video_length(video_path_escaped: str) -> float:
+    return float(
+        run(
+            "ffprobe -v error "
+            "-select_streams v:0 "
+            "-show_entries format=duration -of default=noprint_wrappers=1:nokey=1 "
+            + video_path_escaped,
+            stdout=PIPE,
+            stderr=DEVNULL,
+            text=True,
+            shell=True,
+        ).stdout.strip("\n")
+    )
+
+
+def all_frame_timestame(video_path_escaped: str) -> NDArray[float32]:
+    return array(
+        findall(
+            r"(?<=pts_time:)[0-9\.]++",
+            run(
+                "ffmpeg -i "
+                + video_path_escaped
+                + " -filter:v showinfo -f null -",
+                stdout=DEVNULL,
+                stderr=PIPE,
+                text=True,
+                shell=True,
+            ).stderr,
+        ),
+        dtype=float32,
+    )
+
+
+def keyframe_timestamp(video_path_escaped: str) -> list[float]:
+    return [
+        float(t)
+        for t in findall(
+            r"(?<=pts_time:)[0-9\.]++",
+            run(
+                "ffmpeg -i "
+                + video_path_escaped
+                + " -filter:v \"select='eq(key,1)',showinfo\" -f null -",
+                stdout=DEVNULL,
+                stderr=PIPE,
+                text=True,
+                shell=True,
+            ).stderr,
+        )
+    ]
+
+
+# Get values within boundary
+def extract_between_value(
+    value_list: NDArray[float32], start_inclusive: float, end_exclusive: float
+) -> NDArray[float32]:
+    return value_list[
+        (start_inclusive <= value_list) & (value_list < end_exclusive)
+    ]
+
+
+_ALL_FRAME_START_TIME: Optional[str] = None
+_VIDEO_PATH_ESCAPED: Optional[str] = None
+_WIDTH: Optional[int] = None
+_HEIGHT: Optional[int] = None
+_START_X: Optional[int] = None
+_START_Y: Optional[int] = None
+_END_X: Optional[int | None] = None
+_END_Y: Optional[int | None] = None
+_LANG: Optional[list[str] | None] = None
+
+
+def _init_worker(
+    all_frame_start_time: str,
+    video_path_escaped: str,
+    width: int,
+    height: int,
+    start_x: int,
+    start_y: int,
+    end_x: int | None,
+    end_y: int | None,
+    lang: list[str] | None,
+) -> None:
+    global _ALL_FRAME_START_TIME, _VIDEO_PATH_ESCAPED, _WIDTH, _HEIGHT
+    global _START_X, _START_Y, _END_X, _END_Y, _LANG
+    _ALL_FRAME_START_TIME = all_frame_start_time
+    _VIDEO_PATH_ESCAPED = video_path_escaped
+    _WIDTH = width
+    _HEIGHT = height
+    _START_X = start_x
+    _START_Y = start_y
+    _END_X = end_x
+    _END_Y = end_y
+    _LANG = lang
+
+
+def _timestamps_to_text(
+    timestamps: NDArray[float32],
+) -> tuple[str]:
+    # Get index of frames
+    index: NDArray[intp] = flatnonzero(
+        isin(
+            _ALL_FRAME_START_TIME,
+            timestamps,
+            True,
+            kind="sort",
+        )
+    )
+    # Reset 0 to first frame
+    index = index - index[0]
+    # Get num frames from first frame to last inclusive
+    num_frames: int = index[-1] + 1
+    # Get text
+    texts: Iterator[str] = extract_txt(
+        timestamps[0],
+        _VIDEO_PATH_ESCAPED,
+        _WIDTH,
+        _HEIGHT,
+        num_frames,
+        index,
+        _START_X,
+        _START_Y,
+        _END_X,
+        _END_Y,
+        _LANG,
+    )
+    return tuple(texts)
 
 
 def write_to_file(
@@ -326,54 +474,122 @@ def video2vtt(
                 cue_list.append([start, end, this_cue])
                 pbar.update()
     else:
-        with tqdm(total=num_seg) as pbar:
-            # screenshot_path = f"{dir_temp}/screenshot.png"
-            first_cuenot__entered: bool = True
-            for start, end in zip(time_start, time_end):
-                this_cue: str = next(
-                    extract_txt(
-                        start,
-                        video_path_escaped,
-                        width,
-                        height,
-                        start_x=start_x,
-                        start_y=start_y,
-                        end_x=end_x,
-                        end_y=end_y,
-                        lang=lang,
-                    )
+        keyframe_start_time: list[float, ...] = keyframe_timestamp(
+            video_path_escaped
+        ) + [video_length(video_path_escaped) + 1]
+        if keyframe_start_time[0] != 0:
+            keyframe_start_time = [0] + keyframe_start_time
+
+        all_frame_start_time: NDArray[float32] = all_frame_timestame(
+            video_path_escaped
+        )
+        # Cast to NDArray
+        time_start: NDArray = array(time_start, float32)
+        # Split time_start by keyframe
+        timestamp_list: list[NDArray[float32]] = [
+            time
+            for i in range(1, len(keyframe_start_time))
+            if (
+                time := extract_between_value(
+                    time_start,
+                    keyframe_start_time[i - 1],
+                    keyframe_start_time[i],
                 )
-                if remove != "":
-                    this_cue = sub(remove, replace, this_cue)
-                if this_cue == "":
-                    pbar.update()
-                    continue
-                if first_cuenot__entered:
-                    cue_list: list[float, float, str] = [
-                        [
-                            start,
-                            end,
-                            lyrics_list[0] if use_ltrics else this_cue,
-                        ],
-                    ]
-                    if write_file:
-                        # Write file at interrupt
-                        # Does not work with sub_from_prev
-                        _WRITE_TO_FILE = True
-                        register(write_to_file, video_path, cue_list)
-                    pbar.update()
-                    first_cuenot__entered = False
-                    continue
-                if this_cue == cue_list[-1][2]:
-                    cue_list[-1][1] = end
-                    pbar.update()
-                    continue
-                # cue_list.append([float(start), float(end), pretty(this_cue)])
-                cue_list.append([start, end, this_cue])
-                pbar.update()
+            ).size
+            != 0  # Do nothing if this seg is not used
+        ]
+        del keyframe_start_time
+        # Get time of each frame to extract text from in this seg
+        print("Extracting texts")
+        with ProcessPoolExecutor(
+            3,
+            initializer=_init_worker,
+            initargs=(
+                all_frame_start_time,
+                video_path_escaped,
+                width,
+                height,
+                start_x,
+                start_y,
+                end_x,
+                end_y,
+                lang,
+            ),
+        ) as pool:
+            del (
+                all_frame_start_time,
+                video_path_escaped,
+                width,
+                height,
+                start_x,
+                start_y,
+                end_x,
+                end_y,
+                lang,
+            )
+            with tqdm(total=num_seg) as pbar:
+                workers: list[Future[tuple[NDArray[float32], tuple[str]]]] = [
+                    pool.submit(_timestamps_to_text, timestamps)
+                    for timestamps in timestamp_list
+                ]
+                del timestamp_list
+                # Update pbar
+                for task in as_completed(workers):
+                    pbar.update(len(task.result()))
+
+            # Join all output
+            print("Collecting")
+            all_cue: NDArray[str] = concatenate(
+                [result.result() for result in workers],
+                dtype=object,
+                casting="safe",
+            )
+
+        # Remove all empty cue
+        print("Removing empty cue")
+        mask: NDArrat[bool_] = all_cue != ""
+        time_start = time_start[mask]
+        time_end = array(time_end, dtype=float32)[mask]
+        all_cue = all_cue[mask]
+        del mask
+
+        print("Merging cue")
+        # These 3 masks are 1 less in len along axis 0
+        start_end_conected: NDArray[bool_] = time_start[1:] == time_end[:-1]
+        cue_adj_identical: NDArray[bool_] = all_cue[1:] == all_cue[:-1]
+        connected_and_identical: NDArray[bool_] = (
+            start_end_conected & cue_adj_identical
+        )
+        del start_end_conected, cue_adj_identical
+
+        if all(~connected_and_identical):
+            print("All cue unique")
+            del connected_and_identical
+        else:
+            # Set end time to the end of connected and identical
+            diff_in_state: NDArray[int8] = diff(
+                connected_and_identical.astype(int8), prepend=0, append=0
+            )
+            i_before_con: NDArray[intp] = flatnonzero(diff_in_state == 1)
+            i_end_con: NDArray[intp] = flatnonzero(diff_in_state == -1)
+            # Extend cue end time
+            time_end[i_before_con] = time_end[i_end_con]
+            del diff_in_state, i_before_con, i_end_con
+
+            # Remove all connected and identical cue after the first cue
+            keep: NDArray[bool] = r_[True, ~connected_and_identical]
+            del connected_and_identical
+            time_start = time_start[keep]
+            time_end = time_end[keep]
+            all_cue = all_cue[keep]
+            del keep
+
+        # Merge connected and identical
+        cue_list = tuple(zip(time_start, time_end, all_cue))
 
     # Subtract from cue if it is set
     if sub_from_prev != 0:
+        print("Adjusting end time")
         # Cast to ndarray
         cue_list: NDArray[Tuple[int, 3], object_] = array(cue_list)
         start_time: NDArray[Tuple[int], float] = cue_list[:, 0].astype(float)
@@ -388,6 +604,7 @@ def video2vtt(
 
     # Write to file
     if write_file:
+        print("Writing to file")
         write_to_file(video_path, cue_list)
         _WRITE_TO_FILE = False  # Suppress writing another copy
 
